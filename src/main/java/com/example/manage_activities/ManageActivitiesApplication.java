aba6da98745c35255026da8ac969daba300543ce
NhanTr <truongphannhan1603@gmail.com>
2026-03-19 09:29:50 +0700

    init project

---
+++ src/main/java/com/example/manage_activities/ManageActivitiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.manage_activities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageActivitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageActivitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/manage_activities/ManageActivitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manage_activities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageActivitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
